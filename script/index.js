42ff946f36bce7232d8ae20898295684ac5a06bc
Григорий Лебедев <apple@MacBook-Pro-apple.local>
2022-12-03 23:24:51 +0300

    ready lesson 2 modul 2

---
+++ script/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const title = document.querySelector('.modal__title');
+console.log("🚀 ~ file: index.js:4 ~ title", title);
+
+const form = document.querySelector('.modal__form');
+console.log("🚀 ~ file: index.js:6 ~ form", form);
+
+const checkbox = document.querySelector('.modal__checkbox');
+console.log("🚀 ~ file: index.js:8 ~ checkbox", checkbox);
+
+const checkboxField = document.querySelector('.modal__input_discount');
+console.log("🚀 ~ file: index.js:7 ~ checkboxField", checkboxField);
+
