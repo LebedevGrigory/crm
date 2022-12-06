ce4159271da9adb9069991d494e6dad7dd6e059d
Григорий Лебедев <apple@MacBook-Pro-apple.local>
2022-12-06 23:47:04 +0300

    create lesson 3

--- script/index.js
+++ script/index.js
@@ -1,12 +1,3 @@
 'use strict';
 
 
-const title = document.querySelector('.modal__title');
-const form = document.querySelector('.modal__form');
-const checkbox = document.querySelector('.modal__checkbox');
-const checkboxField = document.querySelector('.modal__input_discount');
-console.log("🚀 ~ file: index.js:7 ~ checkboxField", checkboxField);
-
-console.log("🚀 ~ file: index.js:4 ~ title", title);
-console.log("🚀 ~ file: index.js:6 ~ form", form);
-console.log("🚀 ~ file: index.js:8 ~ checkbox", checkbox); 
